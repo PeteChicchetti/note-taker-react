caca72589856cc1fd4820dd8421d830e66d601dc
unknown <pj0524@gmail.com>
2023-03-09 14:04:55 -0500

    adjusted alignment and backgroudn color of landing page and nav

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <main>
-      <Link to='/signup' style={{ textDecoration: 'none' }}>
+    <main style={{ background: '#414141', margin: '80px 0px 0px 0px', height: 'calc(100vh - 80px)'}}>
+      <Link to='/signup' style={{ textDecoration: 'none', color: 'white' }}>
         <h1>Sign Up now</h1>
       </Link>
-      <Link to='/login' style={{ textDecoration: 'none' }}>
+      <Link to='/login' style={{ textDecoration: 'none', color: 'white' }}>
         <h1>Login now</h1>
       </Link>
     </main>
