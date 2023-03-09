1f5e56a794e5d231f7862f433e93aba15acdedf3
unknown <pj0524@gmail.com>
2023-03-09 15:43:43 -0500

    started building notes component

---
+++ client/src/components/Notes/Notes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from "react-bootstrap";
+import {
+    AddBtn
+} from './NotesElements'
+
+
+const Notes = () => {
+    return (
+        <Container style={{ background: '#414141', margin: '80px 0px 0px 0px', height: 'calc(100vh - 80px)'}}>
+            <AddBtn>Test</AddBtn>
+        </Container>
+    )
+}
+
+export default Notes;
