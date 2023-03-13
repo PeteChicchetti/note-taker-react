abdba6b961e4c39004914c242e1c54686e975370
unknown <pj0524@gmail.com>
2023-03-13 15:29:47 -0400

    add padding top to content and adjusted content page width

--- client/src/components/Notes/NotesElements.jsx
+++ client/src/components/Notes/NotesElements.jsx
@@ -7,8 +7,9 @@ export const NotesMain = styled.div`
 export const NotesContainer = styled.div`
     background: #414141;
     margin: 80px auto 0px;
+    padding-top: 40px;
     height: calc(100vh - 80px);
-    max-width: 1148px;
+    max-width: 1100px;
 `
 
 export const AddBtn = styled.span`
