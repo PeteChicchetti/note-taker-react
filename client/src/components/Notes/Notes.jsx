9a1f6794fe4cb3184001a4c53b0c4b4a2b39dc0e
unknown <pj0524@gmail.com>
2023-03-13 15:37:39 -0400

    added react icon for add note

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { Container } from 'react-bootstrap';
+import { IoIosAddCircle } from "react-icons/io";
 import {
     NotesMain,
     NotesContainer,
@@ -12,7 +13,7 @@ const Notes = () => {
     <>
         <NotesMain>
             <NotesContainer >
-                <AddBtn>Test</AddBtn>
+                <AddBtn><IoIosAddCircle style={{ height: '50px', width: '50px', color: '#000' }} /></AddBtn>
             </NotesContainer>
         </NotesMain>    
     </>  
