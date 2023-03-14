3f01e905b615328b8dc7557a93edeb9c4751837a
unknown <pj0524@gmail.com>
2023-03-14 14:34:15 -0400

    changed color of border and adjusted card container positioning

--- client/src/components/Notes/NotesElements.jsx
+++ client/src/components/Notes/NotesElements.jsx
@@ -9,7 +9,6 @@ export const NotesContainer = styled.div`
     background: #414141;
     margin: 80px auto 0px;
     padding: 24px 24px;
-    
     max-width: 1100px;
 
     @media screen and (max-width: 960px) {
