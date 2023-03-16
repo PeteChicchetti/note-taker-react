5044f94318acdafc65eaed45175b62ebd8c1320d
unknown <pj0524@gmail.com>
2023-03-16 16:17:54 -0400

    added created on date to note card

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -19,6 +19,7 @@ const Notes = () => {
                 <Card className='noteCard'>
                     <h1 className='cardTitle'>What is Lorem Ipsum?</h1>
                     <div className='cardContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                    <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
                 </Card>
             </Container>
         </NotesMain>    
