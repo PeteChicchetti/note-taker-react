b549e38617d3b350d306e1223faaa307ec105759
unknown <pj0524@gmail.com>
2023-03-21 15:58:44 -0400

    setup useState to display note content

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Card } from 'react-bootstrap';
 import { CiStickyNote } from "react-icons/ci";
 import {
@@ -9,6 +9,10 @@ import {
 
 
 const Notes = () => {
+    const [showNote, setShowNote] = useState(false)
+    const onClick = () => setShowNote(true)
+
+
     return (
     <>
     <NotesMain>
@@ -16,8 +20,9 @@ const Notes = () => {
             <Card className='cardMain'>
                 <Title><h1 className='cardTitle'>Lets add some notes!</h1><button className='addBtn'>Add Note</button></Title>
                 <Container className='cardContainer'>
-                    <Card className='noteCard'>
-                        <div className='noteCardHeader'>
+                { showNote ?
+                    <Card className='noteCard' onClick={onClick}>
+                        <div className='noteCardHeader openHeader'>
                             <span className='titleContainer'>
                                 <CiStickyNote className='noteIcon'/>
                                 <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
@@ -27,9 +32,21 @@ const Notes = () => {
                                 <button className='deleteBtn'>DELETE</button>
                             </span>
                         </div>
-                        {/* <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-                        <span className="noteDate">Created on: <span className='date'>March 16th</span></span> */}
+                        <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                        <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
                     </Card>
+                    : <Card className='noteCard' onClick={onClick}>
+                    <div className='noteCardHeader'>
+                        <span className='titleContainer'>
+                            <CiStickyNote className='noteIcon'/>
+                            <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
+                        </span>
+                        <span className='btnContainer'>
+                            <button className='editBtn'>EDIT</button>
+                            <button className='deleteBtn'>DELETE</button>
+                        </span>
+                    </div>
+                </Card> }
                 </Container>
             </Card>
         </NotesContainer>
