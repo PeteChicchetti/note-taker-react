9b0799ff076c74996861a5900137123615164488
unknown <pj0524@gmail.com>
2023-03-22 01:34:37 -0400

    cleaned up code for better readability

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -16,26 +16,27 @@ const Notes = () => {
     return (
     <>
     <NotesMain>
-        <NotesContainer >
-            <Card className='cardMain'>
-                <Title><h1 className='cardTitle'>Lets add some notes!</h1><button className='addBtn'>Add Note</button></Title>
-                <Container className='cardContainer'>
-                { showNote ?
-                    <Card className='noteCard' onClick={() => {open(); close()}}>
-                        <div className='noteCardHeader openHeader'>
-                            <span className='titleContainer'>
-                                <CiStickyNote className='noteIcon'/>
-                                <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
-                            </span>
-                            <span className='btnContainer'>
-                                <button className='editBtn'>EDIT</button>
-                                <button className='deleteBtn'>DELETE</button>
-                            </span>
-                        </div>
-                        <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-                        <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
-                    </Card>
-                    : <Card className='noteCard' onClick={open}>
+    <NotesContainer >
+        <Card className='cardMain'>
+            <Title><h1 className='cardTitle'>Lets add some notes!</h1><button className='addBtn'>Add Note</button></Title>
+            <Container className='cardContainer'>
+            { showNote ?
+                <Card className='noteCard' onClick={() => {open(); close()}}>
+                    <div className='noteCardHeader openHeader'>
+                        <span className='titleContainer'>
+                            <CiStickyNote className='noteIcon'/>
+                            <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
+                        </span>
+                        <span className='btnContainer'>
+                            <button className='editBtn'>EDIT</button>
+                            <button className='deleteBtn'>DELETE</button>
+                        </span>
+                    </div>
+                    <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                    <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
+                </Card>
+                : 
+                <Card className='noteCard' onClick={open}>
                     <div className='noteCardHeader'>
                         <span className='titleContainer'>
                             <CiStickyNote className='noteIcon'/>
@@ -47,9 +48,9 @@ const Notes = () => {
                         </span>
                     </div>
                 </Card> }
-                </Container>
-            </Card>
-        </NotesContainer>
+            </Container>
+        </Card>
+    </NotesContainer>
     </NotesMain>    
     </>  
     )
