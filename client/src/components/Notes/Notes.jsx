9d01ba39d47959b0ed86a290b9ab67eab197a168
unknown <pj0524@gmail.com>
2023-03-23 16:50:22 -0400

    removed edit and delete buttons from note card

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -58,10 +58,6 @@ const Notes = () => {
                             <CiStickyNote className='noteIcon'/>
                             <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
                         </span>
-                        <span>
-                            <button className='editBtn'>EDIT</button>
-                            <button className='deleteBtn'>DELETE</button>
-                        </span>
                     </div>
                     <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
@@ -72,10 +68,6 @@ const Notes = () => {
                         <span className='titleContainer' onClick={open}>
                             <CiStickyNote className='noteIcon'/>
                             <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
-                        </span>
-                        <span className='btnContainer'>
-                            <button className='editBtn'>EDIT</button>
-                            <button className='deleteBtn'>DELETE</button>
                         </span>
                     </div>
                 </Card> 
