62c62491ebb1e65fec7a47d67203aa81c43244bd
unknown <pj0524@gmail.com>
2023-03-23 17:02:34 -0400

    added edit and delete icons to bottom of info bar when note is open

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Container, Card } from 'react-bootstrap';
 import { CiStickyNote } from "react-icons/ci";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
 import {
     NotesMain,
     NotesContainer,
@@ -60,7 +62,10 @@ const Notes = () => {
                         </span>
                     </div>
                     <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-                    <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
+                    <div className='noteInfo'>
+                        <span className='noteBtnContainer'><FaRegEdit className='noteBtns'/><RiDeleteBin6Line  className='noteBtns'/></span>
+                        <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
+                    </div>
                 </Card>
                 : 
                 <Card className='noteCard'>
