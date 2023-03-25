564756c04d0abe434aaca8e58e78e077a45620ad
unknown <pj0524@gmail.com>
2023-03-24 23:07:51 -0400

    added login folder and started component format

---
+++ client/src/components/Login/login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const login = () => {
+
+    return (
+    <>
+        <Card>
+            <form>
+                <input></input>
+                <input></input>
+            </form>
+        </Card>
+    </>
+    )
+};
+
+export default login;
