cfc8a5dd89a23a03bc0245be171bb02c858ee62a
unknown <pj0524@gmail.com>
2023-03-25 21:53:54 -0400

    added login styled components.

---
+++ client/src/components/Login/LoginElements.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LoginMain = styled.div`
+    background: #414141;
+    height: calc(100vh - 80px);
+`;
+
+export const LoginContainer = styled.div`
+    background: #414141;
+    margin: 80px auto 0px;
+    padding: 24px 24px;
+    max-width: 800px;
+
+    @media screen and (max-width: 960px) {
+        padding: 24px 24px;
+    }
+`
--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import {
+    LoginMain,
+    LoginContainer,
+} from './LoginElements'
 
 const Login = () => {
 
     return (
     <>
+    <LoginMain>
+    <LoginContainer>
         <Card className='loginCard'>
             <form>
                 <input></input>
                 <input></input>
             </form>
         </Card>
+    </LoginContainer>
+    </LoginMain>
     </>
     )
 };
