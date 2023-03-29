4c74ac90f5a8be40d17f07d97018d98a0fd25563
unknown <pj0524@gmail.com>
2023-03-28 21:58:20 -0400

    added useState and login mutation to function

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -10,7 +10,10 @@ import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
+
 const Login = () => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
 
     return (
     <>
