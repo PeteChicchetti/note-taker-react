3d5ae61a10266aa26e2723af6e933bf2516dbaf5
unknown <pj0524@gmail.com>
2023-03-30 00:45:09 -0400

    added signup card content and css. missing button and functionality

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -10,8 +10,37 @@ const Signup = () => {
     <>
     <SignupMain>
     <SignupContainer>
-        <Card>
-        
+        <Card className='signupCard'>
+            <form>
+                <h2 className='signupTitle'>SignUp</h2>
+                <div className='signupUsername'>
+                    <h3>Username:</h3>  
+                    <input
+                        className='usernameBox'
+                        placeholder='Username'
+                        name='username'
+                        type='text'
+                    />
+                </div>
+                <div className='signupEmail'>
+                    <h3>Email:</h3>
+                    <input 
+                        className='emailBox' 
+                        placeholder='Email'
+                        name='email'
+                        type='email'
+                    />
+                </div>
+                <div className='signupPassword'>
+                    <h3>Password:</h3>
+                    <input 
+                        className='passwordBox'
+                        placeholder='Password'
+                        name='password'
+                        type='password'
+                    />
+                </div>
+            </form>        
         </Card>
     </SignupContainer>
     </SignupMain>
