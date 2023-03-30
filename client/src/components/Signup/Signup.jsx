fc16d3f2a518b980c61423ae1d8f3c50cc5f1648
unknown <pj0524@gmail.com>
2023-03-30 00:31:10 -0400

    added styled components to signup page

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
+import {
+    SignupMain,
+    SignupContainer,
+} from './SignupElements'
 
 const Signup = () => {
     return (
+    <>
+    <SignupMain>
+    <SignupContainer>
         <Card>
-
+        
         </Card>
+    </SignupContainer>
+    </SignupMain>
+    </>    
+        
     )
 };
 
---
+++ client/src/components/Signup/SignupElements.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const SignupMain = styled.div`
+    background: #414141;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`;
+
+export const SignupContainer = styled.div`
+    background: #414141;
+    margin: 80px auto;
+    max-width: 800px;
+
+    @media screen and (max-width: 960px) {
+        padding: 24px 24px;
+    }
+
+    @media screen and (max-width: 340px) {
+        padding: 10px;
+}
+`
