bed886748958d7f1d33377d4797c70f36ab4e63c
unknown <pj0524@gmail.com>
2023-03-30 00:07:33 -0400

    added signup card and imports

---
+++ client/src/components/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
+
+const Signup = () => {
+    return (
+        <Card>
+
+        </Card>
+    )
+};
+
+export default Signup;
