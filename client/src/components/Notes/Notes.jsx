7f33e45d9ef534325e370955d03760a013ca7203
unknown <pj0524@gmail.com>
2023-03-31 15:43:44 -0400

    refactored code

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -50,7 +50,7 @@ const Notes = () => {
                         <span className='addNoteBtnsMobile'>
                                 <button className='saveBtn'>SAVE</button>
                                 <button className='cancelBtn' onClick={cancel}>CANCEL</button>
-                            </span>
+                        </span>
                     </form>
                 </Card>
                 :
@@ -64,9 +64,14 @@ const Notes = () => {
                             <h2 className='noteTitle'>What is Lorem Ipsum?</h2>
                         </span>
                     </div>
-                    <div className='noteContent'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                    <div className='noteContent'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
                     <div className='noteInfo'>
-                        <span className='noteBtnContainer'><FaRegEdit className='noteBtns'/><RiDeleteBin6Line  className='noteBtns'/></span>
+                        <span className='noteBtnContainer'>
+                            <FaRegEdit className='noteBtns'/>
+                            <RiDeleteBin6Line  className='noteBtns'/>
+                        </span>
                         <span className="noteDate">Created on: <span className='date'>March 16th</span></span>
                     </div>
                 </Card>
