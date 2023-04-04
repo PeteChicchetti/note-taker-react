cfa776ec8f151a55786ecdeb8e7d9335b71a132c
unknown <pj0524@gmail.com>
2023-04-04 14:10:52 -0400

    added post query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -18,3 +18,17 @@ query user($userId: ID!) {
   }
 }
 `;
+
+export const ADD_POST = gql`
+mutation AddPost($title: String!, $content: String!) {
+  addPost(title: $title, content: $content) {
+    _id
+    title
+    content
+    createdAt
+    user {
+      _id
+    }
+  }
+}
+`;
