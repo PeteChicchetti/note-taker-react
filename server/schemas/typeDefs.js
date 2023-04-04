87f5036764a6c5db2bfb31ab8855a418caf8c47c
unknown <pj0524@gmail.com>
2023-04-04 14:46:13 -0400

    updated issue with typedef name

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
 
-    addNote(title: String!, content: String!): Post
+    addNote(title: String!, content: String!): Note
   }
 `;
 
