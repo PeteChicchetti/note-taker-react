eb1d34321607bed12205da68170c52e5f01a5cd3
unknown <pj0524@gmail.com>
2023-04-04 14:17:22 -0400

    added get for notes and add notes resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -41,6 +41,23 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    /// GETS ALL POSTS ///
+    posts: async () => {
+      return await Post.find({}).populate({populate: {path: 'user'}}).populate({path: 'user'}).select('-__v ');
+    },
+    /// ADD POST ///
+    addPost: async (parent, {title, content}, context) => {
+      const post = await Post.create(
+        {title: title, content: content, user: context.user._id}
+      )
+      const updatedUser = await User.findOneAndUpdate(
+        {_id: context.user._id},
+        {$addToSet:{posts: post._id}},
+        {new: true}
+        );
+
+      return  post ;
+    },
   }
 };
 
