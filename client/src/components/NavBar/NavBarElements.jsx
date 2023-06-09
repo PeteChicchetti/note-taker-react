51efe4398f2384543ffb391b298bb3f18baec1ff
unknown <pj0524@gmail.com>
2023-06-09 14:38:51 -0400

    added a border to nav to create a small gap when scrolling

--- client/src/components/NavBar/NavBarElements.jsx
+++ client/src/components/NavBar/NavBarElements.jsx
@@ -12,6 +12,7 @@ export const Nav = styled.nav`
     position: sticky;
     top: 0;
     z-index: 10;
+    border-bottom: 3px solid #414141;
 
     @media screen and (max-width: 960px) {
         transition: 0.8s all ease;
